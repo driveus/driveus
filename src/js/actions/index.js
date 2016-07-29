8b304e5a582643c2e220e3a124a6f4def9de5c45
Ian Mrak <ianmrak@gmail.com>
2016-07-29 15:22:39 -0700

    [Ajax] progress on dummy api call

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,9 +1,29 @@
+const UBER_API_KEY;
+const LYFT_API_KEY;
+const GMAP_API_KEY;
+const UBER_URL;
+const LYFT_URL;
+
+const ROUTE_SELECTED = 'ROUTE_SELECTED';
 export function selectRoute(route) {
   // selectRoute is an ActionCreator
   // it needs to return an action and object with type prop
   console.log('A route has been selected:', route);
   return {
-    type: 'ROUTE_SELECTED',
+    type: ROUTE_SELECTED,
     payload: route
   };
+}
+// use Axios ?!?? npm install --save axios
+// React promise looks cool
+export const FETCH_ROUTES = 'FETCH_ROUTES';
+export function fetchRoutes(location) {
+  const uberUrl; // `${UBER_URL}` plus location query
+  const lyftUrl; // `${LYFT_URL}` plus location query
+  const request; // somehow juggle API calls
+
+  return {
+    type: FETCH_ROUTES,
+    payload: request
+  }
 }
