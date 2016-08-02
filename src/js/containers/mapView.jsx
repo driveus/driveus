34a2dd91ccebfdd1cc36ac4726e5339f5f168826
Ian Mrak <ianmrak@gmail.com>
2016-08-01 17:31:43 -0700

    [Map Makers] working on location markers

--- src/js/containers/mapView.jsx
+++ src/js/containers/mapView.jsx
@@ -5,40 +5,25 @@ import { bindActionCreators } from 'redux';
 class MapView extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      markers: [{
-        position: {
-          lat: 37.7749,
-          lng: -122.4194
-        },
-        key: `USA`,
-        defaultAnimation: 2,
-      }],
-      mapOptions: {
-        scrollwheel: false,
-        lat: 37.7749,
-        lng: -122.4194
-      }
-    }
   }
   componentDidMount() {
-    var map = new google.maps.Map(document.querySelector('.map-container'), {
-      center: { lat: 37.7749, lng: -122.4194 },
+    let map = new google.maps.Map(document.querySelector('.map-container'), {
+      center: this.props.currentLocation.start || { lat: 37.773972, lng: -122.431297 },
       scrollwheel: false,
-      zoom: 14
+      zoom: 10
     });
     this.setState({
       map: map
     });
   }
-  componentWillUpdate() {
-    var startMarker = new google.maps.Marker({
-      position: this.props.currentLocation.start,
+  componentDidUpdate() {
+    let startMarker = new google.maps.Marker({
+      position: this.props.currentLocation.start || null,
       map: this.state.map,
       animation: 2
     });
-    var endMarker = new google.maps.Marker({
-      position: this.props.currentLocation.end,
+    let endMarker = new google.maps.Marker({
+      position: this.props.currentLocation.end || null,
       map: this.state.map,
       animation: 2
     });
