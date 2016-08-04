90266a50d3563e6b826b73f3f1e5e65eb8489120
Ian Mrak <ianmrak@gmail.com>
2016-08-03 18:46:18 -0700

    [Styling] added more sass styling

--- src/js/components/app.jsx
+++ src/js/components/app.jsx
@@ -13,7 +13,7 @@ class App extends Component {
       <div id="wrapper">
         <Header />
         <div id="container">
-          <h1 id="site-title">DrivUs</h1>
+          <h1 id="site-title">DriveUs</h1>
           <Controls />
           <MapView />
           <ActiveRoute />
--- src/js/components/expandSearch.jsx
+++ src/js/components/expandSearch.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 class ExpandSearch extends Component {
   render() {
-    console.log(this.props.expandSearch)
     return (
       <button
         onClick={() => this.props.expandSearch(this.props.currentLocation)}>
