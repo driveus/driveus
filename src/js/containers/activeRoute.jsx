b93b0804a4ccbcc0efe8c05d3e1c5dc6eca8dd58
amol023 <amol5jsh@gmail.com>
2016-08-04 19:45:16 -0700

    [feat] ordering rides

--- src/js/containers/activeRoute.jsx
+++ src/js/containers/activeRoute.jsx
@@ -4,40 +4,40 @@ import { deselectRoute } from '../actions/index.js';
 import { bindActionCreators } from 'redux';
 
 class ActiveRoute extends Component {
-  // constructor(props) {
-  //   super(props)
+  constructor(props) {
+    super(props)
 
-  //   this.orderRide = this.orderRide.bind(this);
-  // }
+    this.orderRide = this.orderRide.bind(this);
+  }
   
-  // orderRide() {
-  //   const lyftRoutes = {
-  //     "Lyft": "lyft",
-  //     "Lyft Line": "lyft_line",
-  //     "Lyft Plus": "lyft_plus"
-  //   }
+  orderRide() {
+    const lyftRoutes = {
+      "Lyft": "lyft",
+      "Lyft Line": "lyft_line",
+      "Lyft Plus": "lyft_plus"
+    }
 
-  //   let startAdd = this.props.currentAddress.start;
-  //   let endAdd = this.props.currentAddress.end;
-  //   let startLat = this.props.currentLocation.start.lat;
-  //   let startLng = this.props.currentLocation.start.lng;
-  //   let endLat = this.props.currentLocation.end.lat;
-  //   let endLng = this.props.currentLocation.end.lng;
-  //   //need to acccess the client id differently
+    let startAdd = this.props.currentAddress.start;
+    let endAdd = this.props.currentAddress.end;
+    let startLat = this.props.currentLocation.start.lat;
+    let startLng = this.props.currentLocation.start.lng;
+    let endLat = this.props.currentLocation.end.lat;
+    let endLng = this.props.currentLocation.end.lng;
+    //need to acccess the client id differently
 
-  //   //add pickup and dropoff coords
-  //   let uberUrl = "https://m.uber.com/ul/?client_id=37yHG1-x8iwme2fjogxoa3wU_4n2vWd5exCpEB8u&action=setPickup";
+    //add pickup  and dropoff coords
+    let uberUrl = "https://m.uber.com/ul/?client_id=37yHG1-x8iwme2fjogxoa3wU_4n2vWd5exCpEB8u&action=setPickup";
 
-  //   let uberCoords = `h&pickup[latitude]=${startLat}&pickup[longitude]=${startLng}&pickup[nickname]=UberHQ&pickup[formatted_address]=1455%20Market%20St%2C%20San%20Francisco%2C%20CA%2094103&dropoff[latitude]=${endLat}&dropoff[longitude]=${endLng}&dropoff[nickname]=Coit%20Tower&dropoff[formatted_address]=1%20Telegraph%20Hill%20Blvd%2C%20San%20Francisco%2C%20CA%2094133&product_id=a1111c8c-c720-46c3-8534-2fcdd730040d`
-  //   //add pickup and destination coords
-  //   let lyftUrl = `lyft://ridetype?id=${lyftRoutes[this.props.route.display_name]}&partner=_2bLC2X8YfE8bVC1qcLa0vOQut5r1lB_`;
+    let uberCoords = `h&pickup[latitude]=${startLat}&pickup[longitude]=${startLng}&pickup[formatted_address]=${encodeURLComponent(startAdd)}&dropoff[latitude]=${endLat}&dropoff[longitude]=${endLng}&dropoff[formatted_address]=${encodeURLComponent(endAdd)}&product_id=a1111c8c-c720-46c3-8534-2fcdd730040d`
+    //add pickup and destination coords
+    let lyftUrl = `lyft://ridetype?id=${lyftRoutes[this.props.route.display_name]}&partner=_2bLC2X8YfE8bVC1qcLa0vOQut5r1lB_`;
 
 
-  //   let lyftCoods = `&pickup[latitude]=${startLat}&pickup[longitude]=${startLng}&destination[latitude]=${endLat}&destination[longitude]=${endLng}`
+    let lyftCoods = `&pickup[latitude]=${startLat}&pickup[longitude]=${startLng}&destination[latitude]=${endLat}&destination[longitude]=${endLng}`
 
-  //   let orderLyft = lyftUrl + lyftCoods;
-  //   console.log(orderLyft)
-  // }
+    let orderLyft = lyftUrl + lyftCoods;
+    console.log(orderLyft)
+  }
   render() {
     // console.log('This is the data I have right now*********',this.props.route.product_id);
 
