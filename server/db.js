4940f222e1d31d28dd9e0747810ed9259f41a519
John Slape <slape.john@gmail.com>
2016-08-10 15:54:48 -0700

    [refactor] Fixed many linting alerts (missing semicolons, etc)

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const massive = require('massive')
+const massive = require('massive');
 const db = massive.connectSync({connectionString: process.env.DB_CONNSTRING});
 
 function saveUber(parsedData, isExpandedSearch) {
@@ -22,7 +22,7 @@ function saveUber(parsedData, isExpandedSearch) {
       if (err) {
         console.log(err);
       }
-    })
+    });
   }
 }
 
@@ -46,7 +46,7 @@ function saveLyft(parsedData, isExpandedSearch) {
       if (err) {
         console.log(err);
       }
-    })
+    });
   }
 }
 module.exports.saveUber = saveUber;
