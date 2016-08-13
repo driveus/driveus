845911ef57e98824e6dc0e282a092a91bf1326ef
elifaz <zafile3@hotmail.com>
2016-08-12 17:37:42 -0700

    [feat] Added current location

--- src/js/containers/controls.jsx
+++ src/js/containers/controls.jsx
@@ -19,20 +19,58 @@ class Controls extends Component {
     };
     this.onFormSubmit = this.onFormSubmit.bind(this);
     this.handleLocationChange = this.handleLocationChange.bind(this);
-    this.handleLocationAutoComplete = this.handleLocationAutoComplete.bind(this)
+    this.handleLocationAutoComplete = this.handleLocationAutoComplete.bind(this);
+    this.setCurrent = this.setCurrent.bind(this);
+    this.updateStartCoords = this.updateStartCoords.bind(this);
   }
   // Gets user location with HTML5 geolocation
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition((position) => {
-      this.setState({
-        currentLocation: {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        }
+  // Retrieves the current coordinates and converts it to actual address, 
+  // it depends on a callback to complete it's run.
+  coordsToAddress(cb) {
+      let geocoder = new google.maps.Geocoder,
+          currentLocation;
+      // Gets user location with HTML5 geolocation
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.setState({
+          currentLocation: {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          }
+        });
+        geocoder.geocode({
+          'location': { 
+            lat: this.state.currentLocation.lat, 
+            lng: this.state.currentLocation.lng
+          }
+        }, (results, status) => {
+          if (status === 'OK') {
+            cb(results[0].formatted_address);
+          } else {
+            window.alert('No results found')
+          }
+        });
       });
-    });
-    document.querySelector('.location-form').addEventListener('keydown', this.handleKeyDown);
+      document.querySelector('.location-form').addEventListener('keydown', this.handleKeyDown);
+    }
+
+  updateStartCoords(address) {
+    this.setState({
+      startLocation: address
+    })
   }
+
+  setCurrent() {
+    this.setState({
+      startLocation: "Retrieving your current location..." 
+    });
+    this.coordsToAddress(this.updateStartCoords)
+  }
+
+  setImage() {
+    let icon = require('../../assets/compass.svg');
+    return icon;
+  }
+
   // Wipes input field after form submission (at the end of redux cycle)
   componentWillReceiveProps() {
     this.setState({
@@ -107,14 +145,20 @@ class Controls extends Component {
       <div className="search-box">
         <form onSubmit={this.onFormSubmit} className="location-form">
           <div className="location-search-inputs">
-            <LocationSearch
-              tripNode="startLocation"
-              onLocationChange={this.handleLocationChange}
-              onAutoComplete={this.handleLocationAutoComplete}
-              value={this.state.startLocation}
-              name="startLocation"
-              placeholder={this.state.startPlaceholder}
-            />
+            <div className="current-location-start">
+              <LocationSearch
+                tripNode="startLocation"
+                className="location-start"
+                onLocationChange={this.handleLocationChange}
+                onAutoComplete={this.handleLocationAutoComplete}
+                value={this.state.startLocation}
+                name="startLocation"
+                placeholder={this.state.startPlaceholder}
+              />
+              <button className="current-location"  onClick={this.setCurrent} >
+                <img src={this.setImage()} /> 
+              </button>
+            </div>
             <LocationSearch
               tripNode="endLocation"
               onLocationChange={this.handleLocationChange}
