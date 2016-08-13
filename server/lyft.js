c187de0848962b7a917bb1a5ee8b7e191a980544
John Slape <slape.john@gmail.com>
2016-08-13 10:52:03 -0700

    [feat] Created coords range around Ferry building with hardcoded surge multiplier (for presentation)

--- server/lyft.js
+++ server/lyft.js
@@ -98,12 +98,14 @@ function parseLyft(apiResponses, isExpandedSearch, city) {
   }
   //*********TESTING AND PRESENTATION ONLY***********
   //Make "Ferry Building Marketplace" always have a surge multiplier
-  if (coords.start.lat === 37.7955805 && coords.start.lng === -122.39341109999998) {
-    for (let ride of rides) {
-      ride.price_multiplier = 1.8;
-      ride.high_estimate *= 1.8;
+  const ferryRange = {lat: [37.79682, 37.79444], lng: [-122.396032, -122.391053]}
+  if (coords.start.lat > ferryRange.lat[1] && coords.start.lat < ferryRange.lat[0]) {
+    if (coords.start.lng < ferryRange.lng[1] && coords.start.lng > ferryRange.lng[0]) {
+      for (let ride of rides) {
+        ride.price_multiplier = 1.8;
+        ride.high_estimate *= 1.8;
+      }
     }
-
   }
   //*********END OF HARDCODED SURGE MULTIPLIER ******
 
