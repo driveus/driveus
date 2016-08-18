35929ae78aa5d4e1b65a5ac9ace07ad6b7b23fbe
Ian Mrak <ianmrak@gmail.com>
2016-08-18 14:01:33 -0700

    [cleanup] Removed old commented code.

--- src/js/components/expandSearch.jsx
+++ src/js/components/expandSearch.jsx
@@ -1,36 +1,6 @@
 import React, { Component } from 'react';
 
 class ExpandSearch extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // sliderValue: 300,
-      // expandedView: false,
-      // searchRadius: null,
-      // previousSearchRadius: null,
-      // routes: false
-    }
-    // this.search = this.search.bind(this);
-    // this.handleSliderChange = this.handleSliderChange.bind(this);
-    // this.handleSearchRadiusClick = this.handleSearchRadiusClick.bind(this);
-    // this.toggleExpandedView = this.toggleExpandedView.bind(this);
-  }
-  componentWillReceiveProps() {
-    // if (this.props.classStyle) {
-    //   this.setState({
-    //     searchRadius: null,
-    //     previousSearchRadius: null
-    //   });
-    // }
-    // if (this.props.routes) {
-    //   this.setState({ routes: true });
-    // }
-  }
-  // handleSearchRadiusClick(e) {
-  //   e.preventDefault();
-  //   this.setState({searchRadius: e.target.value})
-  // }
-  //
   render() {
     if (this.props.message) {
       return (
